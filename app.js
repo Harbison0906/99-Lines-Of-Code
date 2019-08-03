a895653fefadb4574930b52bc1aed692b7a5afeb
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-08-03 14:02:07 -0500

    added condition to change grammar at '1 line of code...'

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let friends = ['Jesse', 'Clark', 'Dub', 'Jake', 'Jonathan'];
 
 for (let i=0; i < friends.length; i++) {
-  for (let x=99; x > 0; x--) {
+  for (let x=99; x >= 1; x--) {
   console.log(x + ' lines of code in the file, ' + x + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + x + ' lines of code in the file.');
+  if (x == 1) {
+    console.log(x + ' line of code in the file, ' + x + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, ' + x +  ' no more lines of code in the file.')
   }
+  }
+  
 }
