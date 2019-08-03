13063a653614c45d2d057c9f7b2b4a0ea97fe321
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-08-03 13:14:06 -0500

    added for loop with another for loop nested within to return song lyrics in console

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 let friends = ['Jesse', 'Clark', 'Dub', 'Jake', 'Jonathan'];
+
+for (let i=0; i < friends.length; i++) {
+  for (let x=99; x > 0; x--) {
+  console.log(x + ' lines of code in the file, ' + x + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + x + ' lines of code in the file.');
+  }
+}
